74e06246ebac7c9cd8ea4808fcf808d9d94ee642
aefebrianto <aefebrianto@waresix.com>
2021-03-08 00:23:50 +0700

    test

--- docker/nginx/dependency/nginx.sh
+++ docker/nginx/dependency/nginx.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 
-apt-get install -y build-essential && \
-wget http://nginx.org/keys/nginx_signing.key && \
-apt-key add nginx_signing.key && \
-cd /etc/apt && \
-echo "deb http://nginx.org/packages/ubuntu focal nginx" > /etc/apt/sources.list && \
-echo "deb-src http://nginx.org/packages/ubuntu focal nginx" > /etc/apt/sources.list && \
-apt-get update && \
-apt-get -y install nginx=1.18.* && \
-service nginx start
+apt-get install -y build-essential
+#apt-get install -y build-essential && \
+#wget http://nginx.org/keys/nginx_signing.key && \
+#apt-key add nginx_signing.key && \
+#cd /etc/apt && \
+#echo "deb http://nginx.org/packages/ubuntu focal nginx" > /etc/apt/sources.list && \
+#echo "deb-src http://nginx.org/packages/ubuntu focal nginx" > /etc/apt/sources.list && \
+#apt-get update && \
+#apt-get -y install nginx=1.18.* && \
+#service nginx start
