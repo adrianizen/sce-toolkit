36c12e7e7586b2913db429a61f94e9ba2d9d917d
aefebrianto <aefebrianto@waresix.com>
2021-03-12 00:01:26 +0700

    check current dir

--- docker/sce/init.sh
+++ docker/sce/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+current_dir=${PWD}
 repo_path="/home/skelflo/sce/sites"
 echo "# Repo Path ${repo_path}"
 
@@ -6,10 +7,11 @@ echo "# Repo Path ${repo_path}"
 eval "$(ssh-agent -s)"
 ssh-add
 
-touch ${repo_path}
+mkdir -p ${repo_path}
 cd ${repo_path} && git clone -b master git@github.com:adrianizen/sce-web.git toolkit --verbose 
 
 # Create Database User
+cd ${current_dir}
 ../../scripts/mysql/create.sh -d sce -u sce
 
 
