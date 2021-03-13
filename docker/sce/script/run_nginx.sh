812b5f19dd83a8b45ad91a8193696e070aaab935
aefebrianto <aefebrianto@waresix.com>
2021-03-14 00:01:47 +0700

    add file

--- docker/sce/script/run_nginx.sh
+++ docker/sce/script/run_nginx.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # copy nginx basic config
-# cp /config/nginx/nginx.conf /etc/nginx/nginx.conf
+cp /config/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # enable site
-# cp -r /config/nginx/sites-conf/* /etc/nginx/sites-enabled/
+cp -r /config/nginx/sites-conf/* /etc/nginx/sites-enabled/
+
+# Start php fpm
+rc-service php-fpm8 start
+rc-service php-cgi8 start
 
 # Start nginx
 rc-service nginx start
