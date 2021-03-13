e446b9c9d9037c2603d294b45afb5c1f75b77f85
aefebrianto <aefebrianto@waresix.com>
2021-03-13 16:46:16 +0700

    delete apt key signing

--- docker/nginx/dependency/nginx.sh
+++ docker/nginx/dependency/nginx.sh
@@ -1,11 +1,7 @@
 #!/bin/bash 
 
 apt-get install -y build-essential && \
-#apt-get install -y wget && \
-wget http://nginx.org/keys/nginx_signing.key && \
-apt-key add nginx_signing.key && \
-echo "deb http://nginx.org/packages/ubuntu focal nginx" > /etc/apt/sources.list && \
-echo "deb-src http://nginx.org/packages/ubuntu focal nginx" > /etc/apt/sources.list && \
+
 apt-get update -yqq && \
 apt-get -y install nginx && \
 mkdir -p /etc/nginx/conf.d && \
