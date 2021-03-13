97b52dfcd4a440de91e04fc4a35d755841b70275
aefebrianto <aefebrianto@waresix.com>
2021-03-13 15:16:27 +0700

    typo .

--- docker/sce/script/run_nginx.sh
+++ docker/sce/script/run_nginx.sh
@@ -4,7 +4,7 @@
 cp /config/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # enable site
-cp -r /config/nginx.sites-conf/* /etc/nginx/sites-enabled/
+cp -r /config/nginx/sites-conf/* /etc/nginx/sites-enabled/
 
 # Start nginx
 rc-service nginx start
