870838897cece1aa0a417b6e58da708652e28abf
aefebrianto <aefebrianto@waresix.com>
2021-03-13 15:14:48 +0700

    delete php8-fpm

--- docker/sce/script/run_nginx.sh
+++ docker/sce/script/run_nginx.sh
@@ -6,8 +6,5 @@ cp /config/nginx/nginx.conf /etc/nginx/nginx.conf
 # enable site
 cp -r /config/nginx.sites-conf/* /etc/nginx/sites-enabled/
 
-# start fpm
-rc-service php8-fpm start
-
 # Start nginx
 rc-service nginx start
