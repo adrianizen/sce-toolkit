5dcd03db6c4c2893dd3bf7ac63ee698af4c4bd71
aefebrianto <aefebrianto@waresix.com>
2021-03-13 22:47:02 +0700

    hmm aneh nih

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-docker-compose up --build -d
-# image_name="skelflo/nginx"
-# base_path="/home/skelflo"
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# service_name=nginx
-# container_base_name=$(echo ${image_name} |sed -E "s/[^a-zA-Z0-9_.-]/_/g")
+# docker-compose up --build -d
+image_name="skelflo/nginx"
+base_path="/home/skelflo"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+service_name=nginx
+container_base_name=$(echo ${image_name} |sed -E "s/[^a-zA-Z0-9_.-]/_/g")
 
-# # set container name
-# i=0
-# container_name=${container_base_name}_$[$i+1]
+# set container name
+i=0
+container_name=${container_base_name}_$[$i+1]
 
-# docker run \
-# --name ${container_name} \
-# -v "${base_path}/${service_name}/log/":"/var/log/nginx/" \
-# -v ${DIR}/config/sites-conf/:"/sites/" \
-# -v ${DIR}/script/:"/script/" \
-# -v /etc/ssl/:"/etc/ssl" \
-# --net=host \
-# -t $image_name \
-#  /bin/bash
+docker run \
+--name ${container_name} \
+-v "${base_path}/${service_name}/log/":"/var/log/nginx/" \
+-v ${DIR}/config/sites-conf/:"/sites/" \
+-v ${DIR}/script/:"/script/" \
+-v /etc/ssl/:"/etc/ssl" \
+--net=host \
+-t $image_name \
+ /bin/bash
