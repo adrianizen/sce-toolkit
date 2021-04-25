65ce19840e88dea2e599877ebba5b4dea1066d28
aefebrianto <aefebrianto@waresix.com>
2021-04-25 22:10:44 +0700

    test npm install

--- docker/adadosa/script/run_application.sh
+++ docker/adadosa/script/run_application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /var/www/html && \
-    npm update && \
+    npm install && \
     npm run prod && \
     composer update --optimize-autoloader --no-dev && \
     php artisan config:cache && \
