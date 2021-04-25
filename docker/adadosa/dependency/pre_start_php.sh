22f3944672d028dff32e40b9da5dbb005b75973d
aefebrianto <aefebrianto@waresix.com>
2021-04-25 22:21:34 +0700

    add php8 session

--- docker/adadosa/dependency/pre_start_php.sh
+++ docker/adadosa/dependency/pre_start_php.sh
@@ -7,7 +7,7 @@ docker-php-ext-install mysqli
 apk add php8-mysqli php8-mbstring php8-curl php8-xml php8-fpm php8-cgi \
     php8-dom php8-exif php8-fileinfo php8-json php8-openssl \
     php8-xml php8-zip php8-zlib \
-    php8-ftp
+    php8-ftp php8-session
 
 echo "cgi.fix_pathinfo = 0;" >> /etc/php8/php.ini
 
