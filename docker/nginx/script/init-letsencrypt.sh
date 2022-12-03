72ecd6c798fd10894bb87651c9033949f8ccb22c
adrian <adrian@luno.com>
2022-12-03 23:22:48 +0700

    add diaryengineer.com

--- docker/nginx/script/init-letsencrypt.sh
+++ docker/nginx/script/init-letsencrypt.sh
@@ -6,7 +6,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 #domains=(adadosa.id www.adadosa.id)
-domains=(adadosa.id pakboy.id selfconsciousengineer.com)
+domains=($1)
 rsa_key_size=4096
 #data_path="./data/certbot"
 data_path="./../config/certbot"
